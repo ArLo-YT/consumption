164f18c8ebebdcdc70a3c483902f467f6526931b
ArLo <rationalitylogic@gmail.com>
2025-09-18 07:00:40 +0800

    Update PIH_ui.py

--- PIH_ui.py
+++ PIH_ui.py
@@ -337,7 +337,7 @@ st.title("消费规划模拟工具")
 op_col1, op_col2 = st.columns([1, 1])
 with op_col1:
     st.subheader("选择一个场景")
-    preset_name = st.selectbox("",
+    preset_name = st.selectbox("也可完全自定义",
         list(PRESETS.keys()), index=0
     )
     if st.button("一键填充该场景", use_container_width=True):
